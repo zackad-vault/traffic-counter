ac6a704613238d459780c13fc04997cb5fe0083b
zackad <zackad.dev@gmail.com>
2018-06-26 17:27:04 +0700

    Counter button component

---
+++ src/components/Counter/Counter.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+class Counter extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {value: 0}
+  }
+
+  clickHandler = () => {
+    this.setState({value: this.state.value + 1})
+  }
+
+  render() {
+    return (
+      <button 
+        onClick={this.clickHandler} 
+        className={`counter-${this.props.index}`}
+      >
+        {this.state.value}
+      </button>
+    )
+  }
+}
+
+export default Counter
