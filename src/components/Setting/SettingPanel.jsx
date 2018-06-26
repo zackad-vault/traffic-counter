7e6849cbf297ffcaf4eb6f0a11f3fb3463c4a348
zackad <zackad.dev@gmail.com>
2018-06-26 21:09:50 +0700

    input for setting row and column

--- src/components/Setting/SettingPanel.jsx
+++ src/components/Setting/SettingPanel.jsx
@@ -1,9 +1,50 @@
 import React from 'react'
 
 class SettingPanel extends React.Component {
+  state = {
+    column: 0,
+    row: 0
+  }
+
+  addColumn = () => {
+    this.setState({
+      column: this.state.column + 1
+    })
+  }
+
+  addRow = () => {
+    this.setState({
+      row: this.state.row + 1
+    })
+  }
+
+  reduceColumn = () => {
+    this.setState({
+      column: this.state.column - 1
+    })
+  }
+
+  reduceRow = () => {
+    this.setState({
+      row: this.state.row - 1
+    })
+  }
+
   render() {
     return (
-      <div>Setting Panel</div>
+      <div>
+        Setting Panel
+        <div>
+          <button className="reduce" onClick={this.reduceColumn}>-</button>
+          <input type="text" value={this.state.column}/>
+          <button className="add" onClick={this.addColumn}>+</button>
+        </div>
+        <div>
+          <button className="reduce" onClick={this.reduceRow}>-</button>
+          <input type="text" value={this.state.row}/>
+          <button className="add" onClick={this.addRow}>+</button>
+        </div>
+      </div>
     )
   }
 }
