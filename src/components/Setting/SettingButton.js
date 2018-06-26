2ec96be28f6000ed443248b57a2b384e2a06f6e0
zackad <zackad.dev@gmail.com>
2018-06-26 17:49:12 +0700

    button and panel setting component

---
+++ src/components/Setting/SettingButton.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class SettingButton extends React.Component {
+  render() {
+    return(
+      <button>Setting</button>
+    )
+  }
+}
+
+export default SettingButton
---
+++ src/components/Setting/SettingPanel.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class SettingPanel extends React.Component {
+  render() {
+    return (
+      <div>Setting Panel</div>
+    )
+  }
+}
+
+export default SettingPanel
