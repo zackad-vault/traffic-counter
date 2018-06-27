d4275a161d2eb0483f5debf3a0c3d77fbc96028a
zackad <zackad.dev@gmail.com>
2018-06-27 10:03:33 +0700

    add key to button component

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -4,11 +4,13 @@ import CounterButton from './CounterButton'
 class Counter extends React.Component {
   render() {
     let counter = []
+    let key
 
     for (let index = 0; index < this.props.column; index++) {
       counter[index] = []
       for (let indexRow = 0; indexRow < this.props.row; indexRow++) {
-        counter[index][indexRow] = <CounterButton />
+        key = index + '-' + indexRow
+        counter[index][indexRow] = <CounterButton key={key}/>
       }
     }
 
