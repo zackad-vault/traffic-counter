30b3dcc152022c904a2d043f09d871a05ae3c4f6
zackad <zackad.dev@gmail.com>
2018-06-27 10:27:41 +0700

    remove un-used variable and component import

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import CounterButton from './CounterButton'
 import CounterRow from './CounterRow'
 
 class Counter extends React.Component {
   render() {
     let counter = []
-    let key
 
     for (let index = 0; index < this.props.column; index++) {
       counter[index] = <CounterRow row={this.props.row} key={index} />
