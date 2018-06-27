953cf8dd3e8110297091a766858d75684c32b596
zackad <zackad.dev@gmail.com>
2018-06-27 10:07:07 +0700

    use parent handler

--- src/components/Setting/SettingPanel.jsx
+++ src/components/Setting/SettingPanel.jsx
@@ -1,48 +1,19 @@
 import React from 'react'
 
 class SettingPanel extends React.Component {
-  state = {
-    column: 0,
-    row: 0
-  }
-
-  addColumn = () => {
-    this.setState({
-      column: this.state.column + 1
-    })
-  }
-
-  addRow = () => {
-    this.setState({
-      row: this.state.row + 1
-    })
-  }
-
-  reduceColumn = () => {
-    this.setState({
-      column: this.state.column - 1
-    })
-  }
-
-  reduceRow = () => {
-    this.setState({
-      row: this.state.row - 1
-    })
-  }
-
   render() {
     return (
       <div>
         Setting Panel
         <div>
-          <button className="reduce" onClick={this.reduceColumn}>-</button>
-          <input type="text" value={this.state.column}/>
-          <button className="add" onClick={this.addColumn}>+</button>
+          <button className="reduce" onClick={this.props.reduceColumn}>-</button>
+          <input type="text" value={this.props.column}/>
+          <button className="add" onClick={this.props.addColumn}>+</button>
         </div>
         <div>
-          <button className="reduce" onClick={this.reduceRow}>-</button>
-          <input type="text" value={this.state.row}/>
-          <button className="add" onClick={this.addRow}>+</button>
+          <button className="reduce" onClick={this.props.reduceRow}>-</button>
+          <input type="text" value={this.props.row}/>
+          <button className="add" onClick={this.props.addRow}>+</button>
         </div>
       </div>
     )
