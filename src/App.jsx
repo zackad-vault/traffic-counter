a05d154269ca0fb1778fbc44699e84aac656fce7
zackad <zackad.dev@gmail.com>
2018-06-28 16:28:13 +0700

    remove unused dependency

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import React, { Component } from 'react'
-// import { set, get } from 'idb-keyval'
 import idbKeyval from './idb-keyval'
-import logo from './logo.svg'
 import './App.css'
 import Counter from './components/Counter/Counter'
 import Setting from './components/Setting/Setting'
@@ -59,13 +57,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
         <Setting
           column={this.state.column}
           addColumn={this.addColumn}
