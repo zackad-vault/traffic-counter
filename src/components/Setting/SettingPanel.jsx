b24b68863f693d5dbff9a9858cae3339b2583f6f
zackad <zackad.dev@gmail.com>
2018-06-28 16:51:16 +0700

    material icon as setting button label

--- src/components/Setting/SettingPanel.jsx
+++ src/components/Setting/SettingPanel.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
+import arrow_drop_down from '../../icons/arrow_drop_down.svg'
+import arrow_drop_up from '../../icons/arrow_drop_up.svg'
+import arrow_left from '../../icons/arrow_left.svg'
+import arrow_right from '../../icons/arrow_right.svg'
 
 class SettingPanel extends React.Component {
   render() {
     return (
       <div>
-        <button className="reduce" onClick={this.props.reduceColumn}>-</button>
-        <input type="text" value={this.props.column} />
-        <button className="add" onClick={this.props.addColumn}>+</button>
-        <button className="reduce" onClick={this.props.reduceRow}>-</button>
-        <input type="text" value={this.props.row} />
-        <button className="add" onClick={this.props.addRow}>+</button>
+        <button className="reduce" onClick={this.props.reduceColumn}><img src={arrow_left} /></button>
+        <span>{this.props.column}</span>
+        <button className="add" onClick={this.props.addColumn}><img src={arrow_right} /></button>
+        <button className="reduce" onClick={this.props.reduceRow}><img src={arrow_drop_down} /></button>
+        <span>{this.props.row}</span>
+        <button className="add" onClick={this.props.addRow}><img src={arrow_drop_up} /></button>
       </div>
     )
   }
