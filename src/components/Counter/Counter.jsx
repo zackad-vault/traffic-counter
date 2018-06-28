87131be58ddb1f016dcdde53a082f243e21dc70e
zackad <zackad.dev@gmail.com>
2018-06-28 09:23:11 +0700

    fix row and column assignment setting

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -5,8 +5,8 @@ class Counter extends React.Component {
   render() {
     let counter = []
 
-    for (let index = 0; index < this.props.column; index++) {
-      counter[index] = <CounterRow row={this.props.row} key={index} />
+    for (let index = 0; index < this.props.row; index++) {
+      counter[index] = <CounterRow row={this.props.column} key={index} />
     }
 
     return <div>{counter}</div>
