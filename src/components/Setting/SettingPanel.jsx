f6331ed998d2c8cc3be65003e6ab52e5f99ec3e0
zackad <zackad.dev@gmail.com>
2018-06-28 16:36:08 +0700

    restructure setting bar

--- src/components/Setting/SettingPanel.jsx
+++ src/components/Setting/SettingPanel.jsx
@@ -4,16 +4,12 @@ class SettingPanel extends React.Component {
   render() {
     return (
       <div>
-        <div>
-          <button className="reduce" onClick={this.props.reduceColumn}>-</button>
-          <input type="text" value={this.props.column}/>
-          <button className="add" onClick={this.props.addColumn}>+</button>
-        </div>
-        <div>
-          <button className="reduce" onClick={this.props.reduceRow}>-</button>
-          <input type="text" value={this.props.row}/>
-          <button className="add" onClick={this.props.addRow}>+</button>
-        </div>
+        <button className="reduce" onClick={this.props.reduceColumn}>-</button>
+        <input type="text" value={this.props.column} />
+        <button className="add" onClick={this.props.addColumn}>+</button>
+        <button className="reduce" onClick={this.props.reduceRow}>-</button>
+        <input type="text" value={this.props.row} />
+        <button className="add" onClick={this.props.addRow}>+</button>
       </div>
     )
   }
