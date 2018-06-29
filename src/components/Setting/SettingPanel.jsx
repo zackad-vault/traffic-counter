75b8bf6d86869d9dd164df791f9bda7c3f3585fb
zackad <zackad.dev@gmail.com>
2018-06-29 09:05:22 +0700

    use camel case

--- src/components/Setting/SettingPanel.jsx
+++ src/components/Setting/SettingPanel.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
-import arrow_drop_down from '../../icons/arrow_drop_down.svg'
-import arrow_drop_up from '../../icons/arrow_drop_up.svg'
-import arrow_left from '../../icons/arrow_left.svg'
-import arrow_right from '../../icons/arrow_right.svg'
+import ArrowDown from '../../icons/arrow_drop_down.svg'
+import ArrowUp from '../../icons/arrow_drop_up.svg'
+import ArrowLeft from '../../icons/arrow_left.svg'
+import ArrowRight from '../../icons/arrow_right.svg'
 
 class SettingPanel extends React.Component {
   render() {
     return (
       <div className="setting__bar">
-        <button className="reduce" onClick={this.props.reduceColumn}><img src={arrow_left} alt="Reduce Column" /></button>
+        <button className="reduce" onClick={this.props.reduceColumn}><img src={ArrowLeft} alt="Reduce Column" /></button>
         <span>{this.props.column}</span>
-        <button className="add" onClick={this.props.addColumn}><img src={arrow_right} alt="Add Column" /></button>
-        <button className="reduce" onClick={this.props.reduceRow}><img src={arrow_drop_down} alt="Reduce Row" /></button>
+        <button className="add" onClick={this.props.addColumn}><img src={ArrowRight} alt="Add Column" /></button>
+        <button className="reduce" onClick={this.props.reduceRow}><img src={ArrowDown} alt="Reduce Row" /></button>
         <span>{this.props.row}</span>
-        <button className="add" onClick={this.props.addRow}><img src={arrow_drop_up} alt="Add Row" /></button>
+        <button className="add" onClick={this.props.addRow}><img src={ArrowUp} alt="Add Row" /></button>
       </div>
     )
   }
