08393b1c6f17b6681ae85ab3b3647f9e6e274019
zackad <zackad.dev@gmail.com>
2019-08-28 13:06:52 +0700

    [refactor] rewrite button styling with tailwind

--- src/components/Counter/CounterButton.jsx
+++ src/components/Counter/CounterButton.jsx
@@ -1,9 +1,8 @@
-import React from "react"
-import './Counter.css'
+import React from 'react'
 
 class CounterButton extends React.Component {
   state = {
-    value: 0
+    value: 0,
   }
 
   clickHandler = () => {
@@ -12,13 +11,13 @@ class CounterButton extends React.Component {
 
   componentWillReceiveProps() {
     if (this.props.reset) {
-      this.setState({value: 0})
+      this.setState({ value: 0 })
     }
   }
 
   render() {
     return (
-      <button onClick={this.clickHandler} className="counter__button">
+      <button onClick={this.clickHandler} className='w-12 h-12 mr-1 mt-1 border'>
         {this.state.value}
       </button>
     )
