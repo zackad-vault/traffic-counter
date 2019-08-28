9d0614146e26c04fdcd0837abce29c0c759424e7
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-28 04:47:09 +0000

    Deploy zackad/traffic-counter to github.com/zackad/traffic-counter.git:gh-pages

---
+++ precache-manifest.96a6833ea763ab273f235e6d90e100c3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3974d740d36459cf0849b25edc072ee",
+    "url": "./index.html"
+  },
+  {
+    "revision": "40709fac923ba908951d",
+    "url": "./static/css/main.a21f2f6b.chunk.css"
+  },
+  {
+    "revision": "c3fb81c0fcebba8e6872",
+    "url": "./static/js/2.1d00926d.chunk.js"
+  },
+  {
+    "revision": "40709fac923ba908951d",
+    "url": "./static/js/main.783e8f02.chunk.js"
+  },
+  {
+    "revision": "eaf83de1abbbe89b4933",
+    "url": "./static/js/runtime~main.6d3fcf46.js"
+  },
+  {
+    "revision": "f131b3f3cbabf1490072d22a77ed920d",
+    "url": "./static/media/arrow_drop_down.f131b3f3.svg"
+  },
+  {
+    "revision": "ec692b43794ed4a7b31430a00adad848",
+    "url": "./static/media/arrow_drop_up.ec692b43.svg"
+  },
+  {
+    "revision": "792f5fffe69287effe54a9684f1c5d5e",
+    "url": "./static/media/arrow_left.792f5fff.svg"
+  },
+  {
+    "revision": "133b4d63b8d24eb247ad9c3254038b4e",
+    "url": "./static/media/arrow_right.133b4d63.svg"
+  },
+  {
+    "revision": "5e5469a1245de321fe54e6d79b2c1867",
+    "url": "./static/media/screen_rotation.5e5469a1.svg"
+  }
+]);
