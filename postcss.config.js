f38da4c7fdaa63cb7a61c16a1e82ef9ec7131eee
zackad <zackad.dev@gmail.com>
2019-08-28 12:40:41 +0700

    [feat] add css optimization with cssnano

--- postcss.config.js
+++ postcss.config.js
@@ -7,5 +7,6 @@ module.exports = {
         content: ['./src/**/*.html', './src/**/*.js', './src/**/*.jsx'],
         defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
       }),
+    process.env.NODE_ENV === 'production' && require('cssnano')({ preset: 'default' }),
   ],
 }
