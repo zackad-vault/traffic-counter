9452dd2dc5061e58df0533aa36c8de34c1125d48
zackad <zackad.dev@gmail.com>
2019-08-28 13:01:55 +0700

    [refactor] rewrite setting panel using tailwind styling

--- src/components/Setting/Setting.jsx
+++ src/components/Setting/Setting.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import SettingPanel from './SettingPanel'
-import './Setting.css'
 
 class Setting extends React.Component {
   render() {
-    return(
-      <div>
+    return (
+      <div className='bg-gray-900 text-white text-xl py-1 flex justify-center'>
         <SettingPanel
           column={this.props.column}
           addColumn={this.props.addColumn}
           reduceColumn={this.props.reduceColumn}
           row={this.props.row}
           addRow={this.props.addRow}
-          reduceRow={this.props.reduceRow} />
+          reduceRow={this.props.reduceRow}
+        />
       </div>
     )
   }
